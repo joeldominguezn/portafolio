82c431b292c20dfcc83caab5d8731dd70ebed6b4
joeldomin06 <joeldomin06@gmail.com>
2025-11-26 15:47:28 -0300

    deploy settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://joeldominguezn.github.io",
+  base: "/portafolio",
   vite: {
     plugins: [tailwindcss()]
   },
