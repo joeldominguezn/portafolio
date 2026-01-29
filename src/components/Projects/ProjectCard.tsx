4667aa49908c47140d8fb7718660ce5132380a90
joeldomin06 <joeldomin06@gmail.com>
2026-01-29 16:54:09 -0300

    add Posnet

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -32,16 +32,16 @@ export default function ProjectCard({item}:Props) {
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                         {item.description}
                     </p>
-                    <div className="flex gap-2 flex-wrap py-2">
+                    <div className="flex flex-col gap-2 flex-wrap py-2">
                         <h3
                             className="text-gray-500 dark:text-gray-300 text-md font-semibold"
                         >
-                            Nivel de Importancia: <span className="text-blue-600 dark:text-blue-400 text-md">{item.priority}</span>
+                            Nivel de Importancia: <span className="text-red-600 dark:text-red-400 text-md">{item.priority}</span>
                         </h3>
                         <h3
                             className="text-gray-500 dark:text-gray-300 text-md font-semibold"
                         >
-                            Creado: <span className="text-blue-600 dark:text-blue-400 text-md">{dayjs(item.createdAt).format("DD/MM/YYYY HH:mm:ss").toString()}</span>
+                            Creado: <span className="text-blue-600 dark:text-blue-400 text-md">{dayjs(item.createdAt).format("DD/MM/YYYY HH:mm:ss").toString().substring(0,10)}</span>
                         </h3>
                     </div>
                     <div className="flex flex-wrap gap-2">
@@ -57,6 +57,7 @@ export default function ProjectCard({item}:Props) {
                     <div className="flex flex-wrap gap-4 py-4">
                         <a
                             href={item.link}
+                            target="_blank"
                             className="inline-flex items-center px-4 py-2 text-sm font-medium text-blue-800 bg-blue-300 border border-blue-300 rounded-lg hover:bg-blue-100 hover:text-blue-600 dark:bg-blue-800 dark:text-blue-300 dark:border-blue-600 dark:hover:text-blue-100 dark:hover:bg-blue-700">
                             Ver el Repositorio
                             <RightArrow className="size-4 ms-2 fill-blue-800 hover:fill-blue-600 dark:fill-blue-300 dark:hover:fill-blue-100" />
@@ -64,6 +65,7 @@ export default function ProjectCard({item}:Props) {
                         {item.liveLink &&
                             <a
                                 href={item.liveLink}
+                                target="_blank"
                                 className="inline-flex items-center px-4 py-2 text-sm font-medium text-sky-800 bg-sky-300 border border-sky-300 rounded-lg hover:bg-sky-100 hover:text-sky-600 dark:bg-sky-800 dark:text-sky-300 dark:border-sky-600 dark:hover:text-sky-100 dark:hover:bg-sky-700">
                                 Ver el Proyecto en Vivo
                                 <RightArrow className="size-4 ms-2 fill-sky-800 hover:fill-sky-600 dark:fill-sky-300 dark:hover:fill-sky-100" />
